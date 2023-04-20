48d744ccc454f810061c2fe33ec6fe8b2ce68901
RobertAzarian <az.roberto1998@gmail.com>
2023-04-20 18:03:02 +0400

    Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,10 +1,23 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                X O X
-                O X O
-                X X O""");//
+        Scanner scanner = new Scanner(System.in);
+        printGrid(scanner.nextLine());
+    }
+
+    public static void printGrid(String input) {
+        char[] v = input.toCharArray();     // values
+        System.out.printf("""
+                ---------
+                | %C %C %C |
+                | %C %C %C |
+                | %C %C %C |
+                ---------""",
+                v[0], v[1], v[2],
+                v[3], v[4], v[5],
+                v[6], v[7], v[8]);
     }
 }
