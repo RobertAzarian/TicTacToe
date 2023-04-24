370b965b6bd326818edc78cabb4606187973db2e
RobertAzarian <az.roberto1998@gmail.com>
2023-04-24 18:56:38 +0400

    Stage 5/5: Fight!

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -7,8 +7,10 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        char playerChar = 'X';
-        String grid = scanner.nextLine();
+        int numberOfMoves = 0;
+        char playerChar;
+        String gameStatus;
+        String grid = "_________";
 
         char[] v = grid.toCharArray();
         char[][] valuesGrid = {
@@ -16,8 +18,14 @@ public class Main {
                 {v[3], v[4], v[5]},
                 {v[6], v[7], v[8]}};
         System.out.println(getGrid(valuesGrid));
-        makeMove(playerChar, valuesGrid);
-        System.out.println(getGrid(valuesGrid));
+        do {
+            numberOfMoves++;
+            playerChar = numberOfMoves % 2 == 0 ? 'O' : 'X';
+            makeMove(playerChar, valuesGrid);
+            System.out.println(getGrid(valuesGrid));
+            gameStatus = getStatus(valuesGrid);
+        } while ("".equals(gameStatus));
+        System.out.println(gameStatus);
     }
 
 
@@ -56,5 +64,101 @@ public class Main {
             }
         } while (true);
         valuesGrid[y-1][x-1] = playerChar;
+    }
+
+
+    public static String getStatus(char[][] valuesGrid) {
+        int countX = 0;
+        int countO = 0;
+        int countSpace = 0;
+        int xWins = 0;
+        int oWins = 0;
+
+        int countInLineX = 0;
+        int countInLineO = 0;
+
+        // horizontal check
+        for (int i = 0; i < valuesGrid.length; i++) {
+            for (int j = 0; j < valuesGrid[i].length; j++) {
+                switch (valuesGrid[i][j]) {
+                    case 'X' -> {
+                        countX++;
+                        countInLineX++;
+                    }
+                    case 'O' -> {
+                        countO++;
+                        countInLineO++;
+                    }
+                    default -> {
+                        countSpace++;
+                    }
+                }
+            }
+            if (countInLineX == 3) {
+                xWins++;
+            } else if (countInLineO == 3) {
+                oWins++;
+            }
+            countInLineX = 0;
+            countInLineO = 0;
+        }
+
+        // vertical check
+        for (int j = 0; j < valuesGrid[0].length; j++) {
+            for (int i = 0; i < valuesGrid.length; i++) {
+                switch (valuesGrid[i][j]) {
+                    case 'X' -> countInLineX++;
+                    case 'O' -> countInLineO++;
+                }
+                if (countInLineX == 3) {
+                    xWins++;
+                } else if (countInLineO == 3) {
+                    oWins++;
+                }
+            }
+            countInLineX = 0;
+            countInLineO = 0;
+        }
+
+        // diagonals check (toRight)
+        for (int i = 0; i < valuesGrid.length; i++) {
+            switch (valuesGrid[i][i]) {
+                case 'X' -> countInLineX++;
+                case 'O' -> countInLineO++;
+            }
+        }
+        if (countInLineX == 3) {
+            xWins++;
+        } else if (countInLineO == 3) {
+            oWins++;
+        }
+        countInLineX = 0;
+        countInLineO = 0;
+
+        // diagonals check (toLeft)
+        for (int i = 2, j = 0; j < valuesGrid.length; i--, j++) {
+            switch (valuesGrid[i][j]) {
+                case 'X' -> countInLineX++;
+                case 'O' -> countInLineO++;
+            }
+        }
+        if (countInLineX == 3) {
+            xWins++;
+        } else if (countInLineO == 3) {
+            oWins++;
+        }
+
+        // return status
+        if (((countX - countO) < -1 || (countX - countO) > 1) || (xWins > 0 && oWins > 0)) {
+            return "Impossible";
+        } else if (countSpace == 0 && (xWins == 0 && oWins == 0)) {
+            return "Draw";
+        } else if (xWins == 1) {
+            return "X wins";
+        } else if (oWins == 1) {
+            return "O wins";
+        } else {
+            return "";
+        }
     }
 }
